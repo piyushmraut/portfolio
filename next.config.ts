46e16bb2137ffc5d4927256d52f8f8b88f3b706c
piyushmraut <pmraut1515@gmail.com>
2025-05-30 20:55:31 +0530

    Fixed Image Issue

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['media.licdn.com', 'avatars.githubusercontent.com'],
+  },
 };
 
 export default nextConfig;
